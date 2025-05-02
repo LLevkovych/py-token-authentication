45c2b671397148e42f1ff31f4c37e63fbc3db7b0
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-02 15:24:02 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -151,13 +151,11 @@ class OrderViewSet(ListView):
     pagination_class = OrderPagination
 
     def get_queryset(self):
-        return self.objects.filter(user=self.request.user)
+        return Order.objects.filter(user=self.request.user)
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return OrderListSerializer
+        return self.queryset.filter(user=self.request.user)
 
-        return OrderSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
